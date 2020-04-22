1e7622c5045e5f758e71a4342f854b1d4b66bab0
athena272 <guilherme.alves073@academico.ifs.edu.br>
2020-04-22 07:51:26 -0300

    parte final desse módulo + tentando deixar mais organizado

--- _1_Comunicao-Serial/_1_Transmitindo-Dados/_ex6_Tudo-junto/_ex6_Tudo-junto.ino
+++ _1_Comunicao-Serial/_1_Transmitindo-Dados/_ex6_Tudo-junto/_ex6_Tudo-junto.ino
@@ -6,14 +6,22 @@
 
 void setup() {
   Serial.begin(9600);     // inicializa comunicação serial (9600 bits/s)
+  //Deixar a tabela de valores um pouco mais bonita, eu acho...
+  Serial.println("DECI| BIN | OCT | HEXA | Char|");
 }
 
 void loop() {
-  delay (100);             // pausa o programa, por 100ms
+  delay (1000);             // pausa o programa, por 100ms
   static byte n = 0;      // declara variável n, do tipo byte, e inicializa com 0
   char c = n;             // declara variável c, do tipo char, e atribui valor de n
   Serial.print ( n );     // imprime o valor como um número (pq???)
-  Serial.print ( " | " );  // imprime uma string ( " | " )
+  Serial.print ( " | " ); // imprime uma string ( " | " )
+  Serial.print( n, BIN); // Imprime n convertido em Binário 
+  Serial.print ( " | " ); // imprime uma string ( " | " )
+  Serial.print(n, OCT); //Imprime n convertido em Octal
+  Serial.print ( " | " ); // imprime uma string ( " | " )
+  Serial.print(n, HEX); //Imprime n convertido em Hexadecimal
+  Serial.print ( " | " ); // imprime uma string ( " | " )
   Serial.println ( c );    // imprime o mesmo valor como um caractere (pq???)
   n++;                    // incrementa valor de n (faz n = n + 1)
 }
